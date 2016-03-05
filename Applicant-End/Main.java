c3bf52a2c9e5833fef7ecddc19abdba558964832
HarishFulara07 <harish14143@iiitd.ac.in>
2016-03-05 22:46:16 +0530

    Set up JavaFx stage and scene

---
+++ Applicant-End/Main.java
@@ -0,0 +1,35 @@
+/*
+ * @author
+ * 
+ * Harish Fulara(2014143)
+ * Anant Mittal(2014015)
+ */
+
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			primaryStage.setTitle("IIIT-Delhi PhD Admissions");
+			Parent root = FXMLLoader.load(getClass().getResource("./PhDAdmissionForm.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
